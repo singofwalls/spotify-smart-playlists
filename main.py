0e238a5c0e05042e20339d839fb058ae42531b8d
Reece Mathews <mathewsreece@gmail.com>
2020-06-11 00:00:20 -0500

    Large refactor to type annotations, documentation, and names Introduced explicit augmented operator handling Replaced lambdas with comprehensions Added progress printing

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """Create dynamic Spotify playlists using Playlist objects."""
+from typing import Iterable, List, Mapping, Dict, Optional
 
 import spotipy
 from spotipy import util
 
 import json
 import operator
-from functools import reduce
 from collections import namedtuple
 
 
@@ -13,6 +13,8 @@ CREDS_FILE = "creds.json"
 
 TRACK_FIELDS = ("id", "name", "is_local")
 PLAYLIST_FIELDS = ("id", "name")
+
+API_LIMIT = 50
 
 
 def main():
@@ -27,19 +29,21 @@ def main():
     p_instrumental = Playlist(spotify, "All Instrumental", populate=True)
 
     p_saved_bands = Playlist(spotify, "Liked Songs - Bands", populate=True)
-    p_saved_bands -= [track for track in p_saved_bands if not track.is_local]
+    remove_nonlocal(p_saved_bands)
     p_saved_bands += p_saved_songs - p_instrumental
     p_saved_bands.name = "Liked Songs - Bands"
     p_saved_bands.publish()
 
-    p_saved_instrumentals = Playlist(spotify, "Liked Songs - Instrumentals", populate=True)
-    p_saved_instrumentals -= [track for track in p_saved_instrumentals if not track.is_local]
+    p_saved_instrumentals = Playlist(
+        spotify, "Liked Songs - Instrumentals", populate=True
+    )
+    remove_nonlocal(p_saved_instrumentals)
     p_saved_instrumentals += p_instrumental & saved_songs
     p_saved_instrumentals.name = "Liked Songs - Instrumentals"
     p_saved_instrumentals.publish()
-    
+
     p_save_songs_all = Playlist(spotify, "Liked Songs - All", populate=True)
-    p_save_songs_all -= [track for track in p_save_songs_all if not track.is_local]
+    remove_nonlocal(p_save_songs_all)
     p_save_songs_all += p_saved_instrumentals + p_saved_bands
     p_save_songs_all.name = "Liked Songs - All"
     p_save_songs_all.publish()
@@ -55,14 +59,14 @@ class Playlist:
         self,
         spotify: spotipy.Spotify,
         name: str,
-        id: str = None,
+        id_: str = None,
         populate: bool = False,
     ):
         self.spotify = spotify
         self.name = name
         self.publish_name = None
         self.tracks: set = set()
-        self.id = id
+        self.id = id_
         if self.id is None:
             self._find_id()
         if self.id is not None and populate:
@@ -85,14 +89,39 @@ class Playlist:
         """Combine tracks of both playlists."""
         return self._membership_op(other, "or", operator.or_)
 
-    def _membership_op(self, other, name, operation):
+    def __iadd__(self, other):
+        """Add tracks from both playlists or tracklist inplace."""
+        # Set addition is really union "or"
+        return self._membership_op(other, "plus", operator.or_, True)
+
+    def __isub__(self, other):
+        """Remove tracks in right playlist from left playlist inplace."""
+        return self._membership_op(other, "minus", operator.sub, True)
+
+    def __iand__(self, other):
+        """Intersect tracks of both playlists inplace."""
+        return self._membership_op(other, "and", operator.and_, True)
+
+    def __ior__(self, other):
+        """Combine tracks of both playlists inplace."""
+        return self._membership_op(other, "or", operator.or_, True)
+
+    def _membership_op(self, other, name, operation, inplace=False):
         """Perform a membership operation on the Playlist."""
-        if type(other) == Playlist:
-            new = self.copy(f" {name} {other.name}")
+        new = self
+        if isinstance(other, Playlist):
+            if inplace:
+                new = self.copy(f" {name} {other.name}")
             new.tracks = operation(new.tracks, other.tracks)
-        elif type(other) in [list, set]:
-            new = self.copy()
-            new.tracks = operation(new.tracks, set(other))
+        else:  # Could check for inplace here to stop non-augmented operations from accepting types other than Playlist
+            if inplace:
+                new = self.copy()
+            try:
+                tracks = set(other)
+            except TypeError:
+                return NotImplemented
+            else:
+                new.tracks = operation(new.tracks, tracks)
         return new
 
     def __bool__(self):
@@ -106,10 +135,6 @@ class Playlist:
     def __iter__(self):
         """Iterate the playlist tracks."""
         return iter(self.tracks)
-
-    def __next__(self):
-        """Get next track in iterable."""
-        yield from self
 
     def copy(self, name_addition=None):
         """Copy tracks into new playlist."""
@@ -138,10 +163,18 @@ class Playlist:
     ):
         """Publish the playlist to spotify."""
 
-        def get_track_ids(tracks):
+        def get_track_ids(tracks: Iterable[Track]):
             """Get the track ids from Track objects."""
-            non_local = filter(lambda t: not t[t._fields.index("is_local")], tracks)
-            return set(map(lambda t: t[t._fields.index("id")], non_local))
+            return set(t.id for t in tracks if not t.is_local)
+
+        def get_track_name(
+            track_id: str, search_list: Iterable[Track]
+        ) -> Optional[str]:
+            """Find a track's name from id in a list of tracks."""
+            for track in search_list:
+                if track.id == track_id:
+                    return track.name
+            return None
 
         name = self.name if name is None else name
 
@@ -151,23 +184,36 @@ class Playlist:
             playlist = self.spotify.user_playlist_create(user, name, public, desc)
             self.id = playlist["id"]
 
-        all_tracks = get_track_ids(self.tracks)
-        current_tracks = get_track_ids(get_playlist_tracks(self.spotify, self.id))
-        new_tracks = list(all_tracks - current_tracks)
-        old_tracks = list(current_tracks - all_tracks)
+        tracks_online = get_playlist_tracks(self.spotify, self.id)
+        tracks_all = list(self.tracks) + tracks_online
 
-        for i in range(0, len(old_tracks), 50):
+        track_ids_current = get_track_ids(tracks_online)
+        track_ids_all = get_track_ids(self.tracks)
+
+        track_ids_new = list(track_ids_all - track_ids_current)
+        tracks_ids_old = list(track_ids_current - track_ids_all)
+
+        track_names_old = [get_track_name(id_, tracks_all) for id_ in tracks_ids_old]
+        print(f"Removing tracks from {name}: {track_names_old}")
+
+        for i in range(0, len(tracks_ids_old), API_LIMIT):
             self.spotify.user_playlist_remove_all_occurrences_of_tracks(
-                user, self.id, old_tracks[i : i + 50]
+                user, self.id, tracks_ids_old[i : i + API_LIMIT]
             )
-        for i in range(0, len(new_tracks), 50):
-            self.spotify.user_playlist_add_tracks(user, self.id, new_tracks[i : i + 50])
+
+        track_names_new = [get_track_name(id_, tracks_all) for id_ in track_ids_new]
+        print(f"Adding tracks to {name}: {track_names_new}")
+        for i in range(0, len(track_ids_new), API_LIMIT):
+            self.spotify.user_playlist_add_tracks(
+                user, self.id, track_ids_new[i : i + API_LIMIT]
+            )
+
+        print("Done.\n")
 
 
-def get_playlist_tracks(spotify: spotipy.Spotify, playlist_id):
+def get_playlist_tracks(spotify: spotipy.Spotify, playlist_id: str) -> List[Track]:
     """Load all songs from the given playlist."""
-    # 50 is max limit for api
-    result = spotify.user_playlist_tracks(None, playlist_id)
+    result = spotify.playlist_tracks(playlist_id)
     results = result["items"]
     while result["next"]:
         result = spotify.next(result)
@@ -178,8 +224,7 @@ def get_playlist_tracks(spotify: spotipy.Spotify, playlist_id):
 
 def get_saved_songs(spotify: spotipy.Spotify):
     """Load all songs from the users saved songs."""
-    # 50 is max limit for api
-    songs = get_all(spotify, spotify.current_user_saved_tracks(limit=50))
+    songs = get_all(spotify, spotify.current_user_saved_tracks(limit=API_LIMIT))
 
     return dicts_to_tracks(select_fields(songs, "track"))
 
@@ -188,7 +233,7 @@ def get_playlists(spotify: spotipy.Spotify):
     """Get a list of user playlist names and ids."""
     playlists = get_all(spotify, spotify.current_user_playlists())
 
-    return select_fields(playlists, None, PLAYLIST_FIELDS)
+    return select_fields(playlists, fields=PLAYLIST_FIELDS)
 
 
 def get_all(spotify: spotipy.Spotify, results: dict):
@@ -223,16 +268,22 @@ def get_spotify(s_creds):
     return spotipy.Spotify(auth=token)
 
 
-def select_fields(items, root=None, fields=TRACK_FIELDS):
+def select_fields(items, root=None, fields=TRACK_FIELDS) -> List[Dict]:
     """Convert api results to dicts."""
     return [
         {k: t[root][k] if root is not None else t[k] for k in fields} for t in items
     ]
 
 
-def dicts_to_tracks(dicts):
+def dicts_to_tracks(dicts: List[Mapping]) -> List[Track]:
     """Convert api result dicts to tracks."""
-    return [Track(*(map(d.get, TRACK_FIELDS))) for d in dicts]
+    return [Track(*(d[field] for field in TRACK_FIELDS)) for d in dicts]
+
+
+def remove_nonlocal(playlist: Playlist):
+    """Remove all nonlocal tracks from the playlist in-place."""
+    playlist -= [track for track in playlist if not track.is_local]
+    return playlist
 
 
 if __name__ == "__main__":
