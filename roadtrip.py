a34d4312d25899a6002b677f30211a528c1bb351
Reece Mathews <mathewsreece@gmail.com>
2020-07-24 03:04:54 -0500

    Create roadtrip playlist generator script

---
+++ roadtrip.py
@@ -0,0 +1,57 @@
+from collections import namedtuple, OrderedDict
+
+import playlists as pl
+
+import random
+
+
+FAMILY_PLAYLISTS = tuple()
+SCALE = 3
+
+creds = pl.get_credentials()
+spotify = pl.get_spotify(creds)
+
+playlists = [pl.Playlist(spotify, id_, id_, True) for id_ in FAMILY_PLAYLISTS]
+cum_wait = OrderedDict((player, 0) for player in playlists)
+
+print(playlists)
+
+playlist_roadtrip = pl.Playlist(spotify, "2020 Family Summer Vacation")
+playlist_roadtrip.publish(public=True)  # Remove all tracks
+songs = []
+
+while playlists:
+    playlist_chosen = random.choices(playlists, cum_weights=tuple(weight**SCALE for weight in cum_wait.values()))[0]
+    song = random.choice(playlist_chosen)
+    playlist_chosen -= song
+    # TODO[reece]: Remove this check once reordering is supported. Dump all playlists into main manually first
+    if song.is_local:
+        continue
+    songs.append(song)
+
+    behind_before = cum_wait[playlist_chosen] - min(cum_wait.values())
+
+    if not playlist_chosen:
+        # Out of songs!
+        playlists.remove(playlist_chosen)
+        del cum_wait[playlist_chosen]
+
+    for playlist in playlists:
+        if playlist is not playlist_chosen:
+            cum_wait[playlist] += song.duration_ms / 1000 / 60
+
+    min_run = min(cum_wait.values()) if playlists else 0
+    for playlist in playlists:
+        cum_wait[playlist] -= min_run
+
+    behind_after = (cum_wait[playlist_chosen] - min(cum_wait.values())) if playlist_chosen in playlists else -1
+    format_p = lambda val: "{:<06.2f}".format(val)
+    format_ = lambda val: "{:<6.2f}".format(val)
+    print("{:<40} {:<3} {:<9} {:<9} {:<9} {:<50}".format(", ".join(format_p(val) for val in cum_wait.values()), FAMILY_PLAYLISTS.index(playlist_chosen.name), format_(song.duration_ms/1000/60), format_(behind_before), format_(behind_after), song.name))
+
+playlist_roadtrip += songs
+print(playlist_roadtrip)
+
+playlist_roadtrip.publish(public=True)
+
+# TODO[reece]: Look for duplicates
